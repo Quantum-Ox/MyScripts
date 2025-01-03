ff9933200f2c11a3a150d4e3c0c4619b56d49d90
Quantum-Ox <larswi@freenet.de>
2025-01-03 11:30:29 +0100

    Now add function to document

---
+++ example1.py
@@ -0,0 +1,12 @@
+
+def func1(arg1,arg2):
+    """
+    This function takes two arguments
+    :param arg1: some value
+    :param arg2: Another one
+
+    :return arg1
+    """
+    arg1 = arg2
+    return arg1
+
